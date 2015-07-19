5c55aa08a3b12bdc61e82b51fc0d857564e91d62
James Hurst <jh.jameshurst@gmail.com>
2015-07-19 15:47:10 -0400

    Added popover

--- HNBar/AppDelegate.swift
+++ HNBar/AppDelegate.swift
@@ -10,18 +10,41 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
+    
+    let popover = NSPopover()
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("togglePopover:")
+        }
+        
+        popover.contentViewController = HackerNewsViewController()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
 
+    func showPopover(sender: AnyObject?) {
+        if let button = statusItem.button {
+            popover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: .MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        popover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if popover.shown {
+            closePopover(sender)
+        }
+        else {
+            showPopover(sender)
+        }
+    }
 
 }
 
---
+++ HNBar/HackerNewsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HackerNewsViewController.swift
+//  HNBar
+//
+//  Created by James on 2015-07-19.
+//  Copyright © 2015 James Hurst. All rights reserved.
+//
+
+import Cocoa
+
+class HackerNewsViewController: NSViewController {
+
+    override var nibName: String? {
+        return "HackerNewsViewController"
+    }
+    
+    override var nibBundle: NSBundle? {
+        return NSBundle.mainBundle()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
