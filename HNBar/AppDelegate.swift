82ce50142a5e05472e533fef2122b766bd031119
James Hurst <jh.jameshurst@gmail.com>
2015-07-19 14:38:55 -0400

    Initial commit

---
+++ HNBar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  HNBar
+//
+//  Created by James on 2015-07-19.
+//  Copyright © 2015 James Hurst. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
